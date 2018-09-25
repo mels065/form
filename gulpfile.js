0129acdcf3815006d4878955ca01d8709a724d27
Brandon Mellus <mellusbrandon@gmail.com>
2018-09-25 12:53:10 -0400

    Create base styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,23 @@ const plugins = require('gulp-load-plugins')();
 const pump = require('pump');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
-const path = require('path');
 
 const SRC_DIR = 'src';
 const DEST_DIR = 'static';
 
-gulp.task('browser-sync', ['nodemon'], () => {
-  browserSync.init({
-    proxy: 'localhost:3000',
-    port: 5000,
-    notify: true
-  });
-});
-
-gulp.task('nodemon', (cb) => {
-  let called = false;
-  return plugins.nodemon({
+gulp.task('nodemon', () => (
+  plugins.nodemon({
     script: 'index.js',
-    ignore: [
-      'gulpfile.js',
-      'node_modules/'
-    ]
+    ext: 'pug scss',
+    env: { 'NODE_ENV': 'development' },
+    tasks: ['pug', 'scss']
   })
-  .on('start', () => {
-    if (!called) {
-      called = true;
-      cb();
-    }
-  })
-  .on('restart', () => {
-    setTimeout(() => reload, 1000)
-  });
-});
+));
 
 gulp.task('pug', () => (
   pump([
-    browserSync.stream()
+    gulp.src(`${SRC_DIR}/views/*.pug`),
+    gulp.dest(`${SRC_DIR}/views`)
   ])
 ));
 
@@ -46,15 +27,14 @@ gulp.task('scss', () => (
   pump([
     gulp.src(`${SRC_DIR}/sass/*.scss`),
     plugins.sassBulkImport(),
-    plugins.sass({ includePaths: ['sass'] }),
+    plugins.sass({ includePaths: ['sass'] }).on('error', plugins.sass.logError),
     plugins.csso(),
-    gulp.dest(`${DEST_DIR}/css`),
-    browserSync.stream()
+    gulp.dest(`${DEST_DIR}/css`)
   ])
+    .on('error', function(err) {
+      console.log(err.toString);
+      this.emit('end');
+    })
 ));
 
-gulp.task('default', ['browser-sync'], () => {
-  gulp.watch(`${SRC_DIR}/sass/**/*.scss`, ['scss']);
-  gulp.watch(`${SRC_DIR}/js/**/*.js`, ['js']);
-  gulp.watch(`${SRC_DIR}/views/**/*.pug`, ['pug']);
-});
+gulp.task('default', ['scss', 'pug', 'nodemon']);
